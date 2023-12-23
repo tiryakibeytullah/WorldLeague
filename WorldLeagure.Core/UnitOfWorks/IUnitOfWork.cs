30d73bcaeb75e5ae29bba1b81eac6e7309ac0998
Beytullah Tiryaki <tiryakibeytullah10@gmail.com>
2023-12-23 14:11:18 +0300

    Unit Of Work Interface yapısı kuruldu.

---
+++ WorldLeagure.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using WorldLeagure.Core.Repositories.DrawReport;
+using WorldLeagure.Core.Repositories.Group;
+using WorldLeagure.Core.Repositories.Team;
+
+namespace WorldLeagure.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IDrawReportReadRepository DrawReportReadRepository { get; }
+        IDrawReportWriteRepository DrawReportWriteRepository { get; }
+        IGroupReadRepository GroupReadRepository { get; }
+        IGroupWriteRepository GroupWriteRepository { get; }
+        ITeamReadRepository TeamReadRepository { get; }
+        ITeamWriteRepository TeamWriteRepository { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
